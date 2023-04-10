42b1a5c53f735f9d79c30eb468f08fa48582c17c
Gabriel <Xthesum41@gmail.com>
2023-04-10 10:12:04 -0300

    JSRS migration

--- code/lib/accessories.js
+++ code/lib/accessories.js
@@ -1,62 +1,75 @@
-module.exports = [
+export default [
   {
-    name: "Foam Tap",
-    details: "A small foam pad placed under the ball of the foot to push the foot up and back if the shoe is too loose."
+    name: 'Foam Tap',
+    details:
+      'A small foam pad placed under the ball of the foot to push the foot up and back if the shoe is too loose.',
   },
   {
-    name: "Heel Grip",
-    details: "Used to prevent the shoe from slipping on the heel if the fit is not perfect."
+    name: 'Heel Grip',
+    details:
+      'Used to prevent the shoe from slipping on the heel if the fit is not perfect.',
   },
   {
-    name: "(Orthopedic) Shoe Insert",
-    details: "Insert of various materials for cushioning, improved fit, or reduced abrasion. These include padding and inner linings. Inserts may also be used to correct foot problems."
+    name: '(Orthopedic) Shoe Insert',
+    details:
+      'Insert of various materials for cushioning, improved fit, or reduced abrasion. These include padding and inner linings. Inserts may also be used to correct foot problems.',
   },
   {
-    name: "Overshoes or Galoshes",
-    details: "A rubber covering placed over shoes for rain and snow protection."
+    name: 'Overshoes or Galoshes',
+    details:
+      'A rubber covering placed over shoes for rain and snow protection.',
   },
   {
-    name: "Shoe Bag",
-    details: "A bag that protects shoes against damage when they are not being worn."
+    name: 'Shoe Bag',
+    details:
+      'A bag that protects shoes against damage when they are not being worn.',
   },
   {
-    name: "Shoe Brush and Polishing Cloth",
-    details: "Used to apply polish to shoes."
+    name: 'Shoe Brush and Polishing Cloth',
+    details: 'Used to apply polish to shoes.',
   },
   {
-    name: "Shoe Polish",
-    details: "A waxy material spread on shoes to improve appearance and glossiness, and provide protection."
+    name: 'Shoe Polish',
+    details:
+      'A waxy material spread on shoes to improve appearance and glossiness, and provide protection.',
   },
   {
-    name: "Shoe Stretcher",
-    details: "A tool for making a shoe longer or wider or for reducing discomfort in areas of a shoe."
+    name: 'Shoe Stretcher',
+    details:
+      'A tool for making a shoe longer or wider or for reducing discomfort in areas of a shoe.',
   },
   {
-    name: "Shoe Tree",
-    details: "Placed inside the shoe when user is not wearing it, to help maintain the shoe's shape."
+    name: 'Shoe Tree',
+    details:
+      "Placed inside the shoe when user is not wearing it, to help maintain the shoe's shape.",
   },
   {
-    name: "Shoehorn",
-    details: "Can be used to insert a foot into a shoe by keeping the shoe open and providing a smooth surface for the foot to slide upon."
+    name: 'Shoehorn',
+    details:
+      'Can be used to insert a foot into a shoe by keeping the shoe open and providing a smooth surface for the foot to slide upon.',
   },
   {
-    name: "Shoelaces",
-    details: "A system used to secure shoes."
+    name: 'Shoelaces',
+    details: 'A system used to secure shoes.',
   },
   {
-    name: "Snow Shoe",
-    details: "A wooden or leather piece that increases the area of ground covered by the shoe."
+    name: 'Snow Shoe',
+    details:
+      'A wooden or leather piece that increases the area of ground covered by the shoe.',
   },
   {
-    name: "Shock Absorbing Plate",
-    details: "An accessory to absorb some of the extra weight when jumping and landing. Good for jump roping."
+    name: 'Shock Absorbing Plate',
+    details:
+      'An accessory to absorb some of the extra weight when jumping and landing. Good for jump roping.',
   },
   {
-    name: "Junior-Sized Shock Absorbing Plate",
-    details: "A smaller sized accessory for smaller feet. Absorbs extra weight from landing."
+    name: 'Junior-Sized Shock Absorbing Plate',
+    details:
+      'A smaller sized accessory for smaller feet. Absorbs extra weight from landing.',
   },
   {
-    name: "Steel Plate",
-    details: "Toe protection, so that you don't accidentally stub your toe and yell out profanity."
-  }
-]
+    name: 'Steel Plate',
+    details:
+      "Toe protection, so that you don't accidentally stub your toe and yell out profanity.",
+  },
+];
--- code/lib/shoes.js
+++ code/lib/shoes.js
@@ -1,261 +1,277 @@
-module.exports = [{
-    name: "Boots",
-    description: "Covers the foot and the ankle and extends up the leg.",
+export default [
+  {
+    name: 'Boots',
+    description: 'Covers the foot and the ankle and extends up the leg.',
     price: {
       value: 70,
       currencyType: {
-        currencyCode: "USD",
-        prefixSymbol: "$"
-      }
+        currencyCode: 'USD',
+        prefixSymbol: '$',
+      },
     },
-    images: [{url:'/images/boot.jpg'}],
-    type: "Boot"
+    images: [{ url: '/images/boot.jpg' }],
+    type: 'Boot',
   },
   {
-    name: "High Heels",
-    description: "Raised heels, by 2 inches (5 cm) above the toes, commonly worn by women for formal occasions or social outings.",
+    name: 'High Heels',
+    description:
+      'Raised heels, by 2 inches (5 cm) above the toes, commonly worn by women for formal occasions or social outings.',
     price: {
       value: 85,
       currencyType: {
-        currencyCode: "USD",
-        prefixSymbol: "$"
-      }
+        currencyCode: 'USD',
+        prefixSymbol: '$',
+      },
     },
-    images: [{url:'/images/heels.jpg'}],
-    type: "Formal"
+    images: [{ url: '/images/heels.jpg' }],
+    type: 'Formal',
   },
   {
-    name: "Mules",
-    description: "No fitting around the heel (i.e. they are backless).",
+    name: 'Mules',
+    description: 'No fitting around the heel (i.e. they are backless).',
     price: {
       value: 65,
       currencyType: {
-        currencyCode: "USD",
-        prefixSymbol: "$"
-      }
+        currencyCode: 'USD',
+        prefixSymbol: '$',
+      },
     },
-    images: [{url:'/images/mules.jpg'}],
-    type: "Formal"
+    images: [{ url: '/images/mules.jpg' }],
+    type: 'Formal',
   },
   {
-    name: "Flip Flops",
-    description: "Type of sandal typically worn as a form of casual wear. They consist of a flat sole held loosely on the foot by a Y-shaped strap that passes between the first and second toes and around both sides of the foot.",
+    name: 'Flip Flops',
+    description:
+      'Type of sandal typically worn as a form of casual wear. They consist of a flat sole held loosely on the foot by a Y-shaped strap that passes between the first and second toes and around both sides of the foot.',
     price: {
       value: 30,
       currencyType: {
-        currencyCode: "USD",
-        prefixSymbol: "$"
-      }
+        currencyCode: 'USD',
+        prefixSymbol: '$',
+      },
     },
-    images: [{url:'/images/flipflops.jpg'}],
-    type: "Sandals"
+    images: [{ url: '/images/flipflops.jpg' }],
+    type: 'Sandals',
   },
   {
-    name: "Sandals",
-    description: "Open shoes consisting of a sole and various straps, leaving much of the foot exposed to air. They are thus popular for warm-weather wear, because they let the foot be cooler than a closed-toed shoe would.",
+    name: 'Sandals',
+    description:
+      'Open shoes consisting of a sole and various straps, leaving much of the foot exposed to air. They are thus popular for warm-weather wear, because they let the foot be cooler than a closed-toed shoe would.',
     price: {
       value: 35,
       currencyType: {
-        currencyCode: "USD",
-        prefixSymbol: "$"
-      }
+        currencyCode: 'USD',
+        prefixSymbol: '$',
+      },
     },
-    images: [{url:'/images/sandals.jpg'}],
-    type: "Sandals"
+    images: [{ url: '/images/sandals.jpg' }],
+    type: 'Sandals',
   },
   {
-    name: "Oxfords",
-    description: "The shoe vamp has a V-shaped slit to which the laces are attached; also known as closed lacing.",
+    name: 'Oxfords',
+    description:
+      'The shoe vamp has a V-shaped slit to which the laces are attached; also known as closed lacing.',
     price: {
       value: 45,
       currencyType: {
-        currencyCode: "USD",
-        prefixSymbol: "$"
-      }
+        currencyCode: 'USD',
+        prefixSymbol: '$',
+      },
     },
-    images: [{url:'/images/oxfords.jpg'}],
-    type: "Casual"
+    images: [{ url: '/images/oxfords.jpg' }],
+    type: 'Casual',
   },
   {
-    name: "Derby",
-    description: "The laces are tied to two pieces of leather independently attached to the vamp; also known as open lacing and is a step down in dressiness.",
+    name: 'Derby',
+    description:
+      'The laces are tied to two pieces of leather independently attached to the vamp; also known as open lacing and is a step down in dressiness.',
     price: {
       value: 40,
       currencyType: {
-        currencyCode: "USD",
-        prefixSymbol: "$"
-      }
+        currencyCode: 'USD',
+        prefixSymbol: '$',
+      },
     },
-    images: [{url:'/images/derby.jpg'}],
-    type: "Casual"
+    images: [{ url: '/images/derby.jpg' }],
+    type: 'Casual',
   },
   {
-    name: "Slip-ons",
-    description: "There are no lacings or fastenings.",
+    name: 'Slip-ons',
+    description: 'There are no lacings or fastenings.',
     price: {
       value: 35,
       currencyType: {
-        currencyCode: "USD",
-        prefixSymbol: "$"
-      }
+        currencyCode: 'USD',
+        prefixSymbol: '$',
+      },
     },
-    images: [{url:'/images/slipon.jpg'}],
-    type: "Casual"
+    images: [{ url: '/images/slipon.jpg' }],
+    type: 'Casual',
   },
   {
-    name: "Bowling",
-    description: "These have harder rubber soles designed specifically not to damage bowling alleys.",
+    name: 'Bowling',
+    description:
+      'These have harder rubber soles designed specifically not to damage bowling alleys.',
     price: {
       value: 60,
       currencyType: {
-        currencyCode: "USD",
-        prefixSymbol: "$"
-      }
+        currencyCode: 'USD',
+        prefixSymbol: '$',
+      },
     },
-    images: [{url:'/images/bowling.jpg'}],
-    type: "Athletic"
+    images: [{ url: '/images/bowling.jpg' }],
+    type: 'Athletic',
   },
   {
-    name: "Golf",
-    description: "These have spikes for better grip in grass terrain.",
+    name: 'Golf',
+    description: 'These have spikes for better grip in grass terrain.',
     price: {
       value: 70,
       currencyType: {
-        currencyCode: "USD",
-        prefixSymbol: "$"
-      }
+        currencyCode: 'USD',
+        prefixSymbol: '$',
+      },
     },
-    images: [{url:'/images/golf.jpg'}],
-    type: "Athletic"
+    images: [{ url: '/images/golf.jpg' }],
+    type: 'Athletic',
   },
   {
-    name: "Hiking",
-    description: "These have stiff upper; offer good ankle support even on uneven terrain and good traction of the sole.",
+    name: 'Hiking',
+    description:
+      'These have stiff upper; offer good ankle support even on uneven terrain and good traction of the sole.',
     price: {
       value: 75,
       currencyType: {
-        currencyCode: "USD",
-        prefixSymbol: "$"
-      }
+        currencyCode: 'USD',
+        prefixSymbol: '$',
+      },
     },
-    images: [{url:'/images/hiking.jpg'}],
-    type: "Athletic"
+    images: [{ url: '/images/hiking.jpg' }],
+    type: 'Athletic',
   },
   {
-    name: "Soccer",
-    description: "These have cleats that are made typically for use on natural surfaces such as dirt and grass. These cleats are equipped with large studs on the bottom of the shoe to assist in gripping the surface and preventing sliding and assisting in rapid directional changes. These studs are permanently attached to the cleats (i.e. they are not removable).",
+    name: 'Soccer',
+    description:
+      'These have cleats that are made typically for use on natural surfaces such as dirt and grass. These cleats are equipped with large studs on the bottom of the shoe to assist in gripping the surface and preventing sliding and assisting in rapid directional changes. These studs are permanently attached to the cleats (i.e. they are not removable).',
     price: {
       value: 55,
       currencyType: {
-        currencyCode: "USD",
-        prefixSymbol: "$"
-      }
+        currencyCode: 'USD',
+        prefixSymbol: '$',
+      },
     },
-    images: [{url:'/images/soccer.jpg'}],
-    type: "Athletic"
+    images: [{ url: '/images/soccer.jpg' }],
+    type: 'Athletic',
   },
   {
-    name: "Wet Weather Soccer",
-    description: "These have soft ground cleats which are made for wet weather. The soft ground cleats are always replaceable and are metal so when they wear down they are easy to replace.",
+    name: 'Wet Weather Soccer',
+    description:
+      'These have soft ground cleats which are made for wet weather. The soft ground cleats are always replaceable and are metal so when they wear down they are easy to replace.',
     price: {
       value: 60,
       currencyType: {
-        currencyCode: "USD",
-        prefixSymbol: "$"
-      }
+        currencyCode: 'USD',
+        prefixSymbol: '$',
+      },
     },
-    images: [{url:'/images/soccer_wet.jpg'}],
-    type: "Athletic"
+    images: [{ url: '/images/soccer_wet.jpg' }],
+    type: 'Athletic',
   },
   {
-    name: "Turf Soccer",
-    description: "Turf shoes have identical uppers to traditional soccer boots, but the outsoles differ in that they make up the cleat portion. Small rubber studs or patterns are like raised bumps that provide just enough of a difference in height to aid in traction on flat artificial turf. They are extremely durable, as they are more of an extension of the rubber outsole than a completely new addition to the shoe.",
+    name: 'Turf Soccer',
+    description:
+      'Turf shoes have identical uppers to traditional soccer boots, but the outsoles differ in that they make up the cleat portion. Small rubber studs or patterns are like raised bumps that provide just enough of a difference in height to aid in traction on flat artificial turf. They are extremely durable, as they are more of an extension of the rubber outsole than a completely new addition to the shoe.',
     price: {
       value: 75,
       currencyType: {
-        currencyCode: "USD",
-        prefixSymbol: "$"
-      }
+        currencyCode: 'USD',
+        prefixSymbol: '$',
+      },
     },
-    images: [{url:'/images/soccer_turf.jpg'}],
-    type: "Athletic"
+    images: [{ url: '/images/soccer_turf.jpg' }],
+    type: 'Athletic',
   },
   {
-    name: "Jazz",
-    description: "Two-part rubberized sole to provide both flexibility and traction, and a short heel. They are secured to the foot by laces or elastic inserts.",
+    name: 'Jazz',
+    description:
+      'Two-part rubberized sole to provide both flexibility and traction, and a short heel. They are secured to the foot by laces or elastic inserts.',
     price: {
       value: 65,
       currencyType: {
-        currencyCode: "USD",
-        prefixSymbol: "$"
-      }
+        currencyCode: 'USD',
+        prefixSymbol: '$',
+      },
     },
-    images: [{url:'/images/jazz.jpg'}],
-    type: "Dance"
+    images: [{ url: '/images/jazz.jpg' }],
+    type: 'Dance',
   },
   {
-    name: "Ghillies",
-    description: "Soft shoes ideal for Irish dance!",
+    name: 'Ghillies',
+    description: 'Soft shoes ideal for Irish dance!',
     price: {
       value: 50,
       currencyType: {
-        currencyCode: "USD",
-        prefixSymbol: "$"
-      }
+        currencyCode: 'USD',
+        prefixSymbol: '$',
+      },
     },
-    images: [{url:'/images/ghillies.jpg'}],
-    type: "Dance"
+    images: [{ url: '/images/ghillies.jpg' }],
+    type: 'Dance',
   },
   {
-    name: "Ballet",
-    description: "Soft, pliable shoes made of canvas with continuous sole used for ballet dancing. The sole is made of leather with thicker material under the ball and heel of the foot, and thinner and thus more flexible material under the arch so that the foot can be easily pointed. They are secured by elastics across the top of the foot.",
+    name: 'Ballet',
+    description:
+      'Soft, pliable shoes made of canvas with continuous sole used for ballet dancing. The sole is made of leather with thicker material under the ball and heel of the foot, and thinner and thus more flexible material under the arch so that the foot can be easily pointed. They are secured by elastics across the top of the foot.',
     price: {
       value: 88,
       currencyType: {
-        currencyCode: "USD",
-        prefixSymbol: "$"
-      }
+        currencyCode: 'USD',
+        prefixSymbol: '$',
+      },
     },
-    images: [{url:'/images/ballet.jpg'}],
-    type: "Dance"
+    images: [{ url: '/images/ballet.jpg' }],
+    type: 'Dance',
   },
   {
-    name: "Pointe",
-    description: "Designed for ballet dancing. These have a toe box that is stiffened with glue and a hardened sole so the dancer can stand on the tips of their toes. They are secured by elastic straps and ribbons that are tied to the dancer's ankles.",
+    name: 'Pointe',
+    description:
+      "Designed for ballet dancing. These have a toe box that is stiffened with glue and a hardened sole so the dancer can stand on the tips of their toes. They are secured by elastic straps and ribbons that are tied to the dancer's ankles.",
     price: {
       value: 80,
       currencyType: {
-        currencyCode: "USD",
-        prefixSymbol: "$"
-      }
+        currencyCode: 'USD',
+        prefixSymbol: '$',
+      },
     },
-    images: [{url:'/images/pointe.jpg'}],
-    type: "Dance"
+    images: [{ url: '/images/pointe.jpg' }],
+    type: 'Dance',
   },
   {
-    name: "Tap",
-    description: "These have metal plates mounted to the bottoms of the toe and heel. The metal plates, which are known as taps, make a loud sound when struck against a hard performance surface. Tap shoes, which are used in tap dancing, may be made from any style of shoe to which taps can be attached.",
+    name: 'Tap',
+    description:
+      'These have metal plates mounted to the bottoms of the toe and heel. The metal plates, which are known as taps, make a loud sound when struck against a hard performance surface. Tap shoes, which are used in tap dancing, may be made from any style of shoe to which taps can be attached.',
     price: {
       value: 95,
       currencyType: {
-        currencyCode: "USD",
-        prefixSymbol: "$"
-      }
+        currencyCode: 'USD',
+        prefixSymbol: '$',
+      },
     },
-    images: [{url:'/images/tap.jpg'}],
-    type: "Dance"
+    images: [{ url: '/images/tap.jpg' }],
+    type: 'Dance',
   },
   {
-    name: "Basketball",
-    description: "For maximum performance playing basketball. The cushioning is designed for jumping very high and landing after a slam dunk. High tops are designed for pivoting your feet and dribbling.",
+    name: 'Basketball',
+    description:
+      'For maximum performance playing basketball. The cushioning is designed for jumping very high and landing after a slam dunk. High tops are designed for pivoting your feet and dribbling.',
     price: {
       value: 110,
       currencyType: {
-        currencyCode: "USD",
-        prefixSymbol: "$"
-      }
+        currencyCode: 'USD',
+        prefixSymbol: '$',
+      },
     },
-    images: [{url:'/images/basketball.jpg'}],
-    type: "Athletic"
-  }
-]
+    images: [{ url: '/images/basketball.jpg' }],
+    type: 'Athletic',
+  },
+];
